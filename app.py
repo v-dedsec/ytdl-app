32028400c9004ba660bce3abfef8991daad7d182
v-dedsec <vv9569117@gmail.com>
2026-04-26 13:28:33 +0530

    changes

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def download(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
